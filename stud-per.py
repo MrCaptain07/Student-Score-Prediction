fc120277b805ced0dbf0ebfb35c481a784e2c8e0
Hrishabh Tomar <hrishabhsinght@gmail.com>
2025-08-12 00:39:12 -0400

    updated file

--- stud-per.py
+++ stud-per.py
@@ -3,6 +3,14 @@ import pandas as pd
 import numpy as np
 import pickle
 from sklearn.preprocessing import StandardScaler,LabelEncoder
+import pymongo
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://sudhanshu:78907890bB@cluster0.ne5qbyw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+client = MongoClient(uri, server_api=ServerApi('1'))
+db = client["student"]
+collection = db["student_pred"]
 
 
 def load_model():
@@ -42,8 +50,11 @@ def main():
         }
         prediction = predict_data(user_data)
         st.success(f"your prediciotn result is {prediction}")
+        user_data['prediction'] = round(float(prediction[0]),2)
+        user_data = {key: int(value) if isinstance(value, np.integer) else float(value) if isinstance(value, np.floating) else value for key, value in user_data.items()}
+        collection.insert_one(user_data)
     
 if __name__ == "__main__":
-    main()
+    main() 
     
 
